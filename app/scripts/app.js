c2dd282d11a10fae502072bd8aa0af12f2461da7
Daniel Burnside <dburnside22@gmail.com>
2018-02-07 13:28:52 -0500

    Attach Angular and make home ctrl and home template

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+  function config($locationProvider, $stateProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+              });
+
+         $stateProvider
+             .state('home', {
+                 url: '/',
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
+     }
+
+    angular
+        .module('portfolio', ['ui.router'])
+        .config(config);
+})();
